237529a3a68aa8a4932c687e17e5837ce4994fb8
bacElmore <71746770+bacElmore@users.noreply.github.com>
2022-02-14 14:54:04 -0800

    Implemented Threading into ColorDetection

--- ColorDetection.py
+++ ColorDetection.py
@@ -1,70 +1,64 @@
-import cv2
-import time
-from djitellopy import Tello
+from threading import Thread
+import cv2, time
 import numpy as np
-url = "rtsp://192.168.10.228:8554/live"
-ipCamURL = 'rtsp://admin:elmore@10.1.10.53:554/ch02/01'
 
-
-# tello = Tello()
-# tello.connect()
-#
-# tello.streamon()
-# frame_read = tello.get_frame_read()
-
+#Frame Size
 width = 1280
 height = 720
 
-#cap = cv2.VideoCapture(ipCamURL)
-cap = cv2.VideoCapture(url)
-#cap = cv2.VideoCapture(0)
+# RED
+lowRed_HSV1 = np.array([0, 70, 50])
+highRed_HSV1 = np.array([10, 255, 255])
+lowRed_HSV2 = np.array([170, 70, 50])
+highRed_HSV2 = np.array([180, 255, 255])
 
-#cap.set(3, width)
-#cap.set(4, height)
+class VideoStreamWidget(object):
+    def __init__(self, src=0):
 
-while True:
-    startTime = time.time()
-    camera, frame = cap.read()
-    #frame = frame_read.frame
+        self.capture = cv2.VideoCapture(src)
+        self.capture.set(3, width)
+        self.capture.set(4, height)
+        # Start the thread to read frames from the video stream
+        self.thread = Thread(target=self.update, args=())
+        self.thread.daemon = True
+        self.thread.start()
 
-    FPS = int(1/(time.time()-startTime))
-    #FPS = 0
+    def update(self):
+        # Read the next frame from the stream in a different thread
+        while True:
+            if self.capture.isOpened():
+                start_time = time.time()
+                (self.status, self.frame) = self.capture.read()
+                self.blurredFrame = cv2.GaussianBlur(self.frame, (5, 5), 0)
+                self.HSVFrame = cv2.cvtColor(self.blurredFrame, cv2.COLOR_BGR2HSV)
+                self.red_mask1 = cv2.inRange(self.HSVFrame, lowRed_HSV1, highRed_HSV1)
+                self.red_mask2 = cv2.inRange(self.HSVFrame, lowRed_HSV2, highRed_HSV2)
 
-    if frame is not None:
+                self.redFrame = cv2.bitwise_and(self.frame, self.frame, mask=self.red_mask1)
+                self.red_mask = self.red_mask1 | self.red_mask2
+                self.contours, self.hierarchy = cv2.findContours(self.red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+                for contour in self.contours:
+                    if cv2.contourArea(contour) > 1000:
+                        x, y, w, h = cv2.boundingRect(contour)
+                        cv2.rectangle(self.frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                FPS = int(1.0/(time.time()-start_time))
+                self.frame = cv2.putText(self.frame, f'FPS: {FPS}', [width - 120, 30], cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+            time.sleep(.01)
 
-        #frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
-        blurredFrame = cv2.GaussianBlur(frame, (5, 5), 0)
-        HSVframe = cv2.cvtColor(blurredFrame, cv2.COLOR_BGR2HSV)
-
-        #RED
-        lowRed_HSV1 = np.array([0,70,50])
-        highRed_HSV1 = np.array([10, 255, 255])
-        lowRed_HSV2 = np.array([170,70,50])
-        highRed_HSV2 = np.array([180, 255, 255])
-
-
-        #mask = cv2.inRange(frame, lowRed, highRed)
-        red_mask1 = cv2.inRange(HSVframe, lowRed_HSV1, highRed_HSV1)
-        red_mask2 = cv2.inRange(HSVframe, lowRed_HSV2, highRed_HSV2)
-
-        redFrame = cv2.bitwise_and(frame, frame, mask=red_mask1)
-        red_mask = red_mask1 | red_mask2
-
-        contours, hierarchy = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        for contour in contours:
-            area = cv2.contourArea(contour)
-            if area > 1000:
-                x, y, w, h = cv2.boundingRect(contour)
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-        #cv2.drawContours(frame, contours, -1, (0,255,0),3)
-
-        frame = cv2.putText(frame, f'FPS: {FPS}', [width-50, 30], cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-        cv2.imshow('testIpCam', redFrame)
-        cv2.imshow('Mask Red', frame)
+    def show_frame(self):
+        # Display frames in main program
+        cv2.imshow('frame', self.frame)
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            self.capture.release()
+            cv2.destroyAllWindows()
+            exit(1)
 
 
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
-
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    video_stream_widget = VideoStreamWidget()
+    while True:
+        try:
+            video_stream_widget.show_frame()
+        except AttributeError:
+            pass
--- TestThreading.py
+++ TestThreading.py
@@ -1,22 +1,64 @@
-import threading
-import time
+from threading import Thread
+import cv2, time
+import numpy as np
+
+#Frame Size
+width = 1280
+height = 720
+
+# RED
+lowRed_HSV1 = np.array([0, 70, 50])
+highRed_HSV1 = np.array([10, 255, 255])
+lowRed_HSV2 = np.array([170, 70, 50])
+highRed_HSV2 = np.array([180, 255, 255])
+
+class VideoStreamWidget(object):
+    def __init__(self, src=0):
+
+        self.capture = cv2.VideoCapture(src)
+        self.capture.set(3, width)
+        self.capture.set(4, height)
+        # Start the thread to read frames from the video stream
+        self.thread = Thread(target=self.update, args=())
+        self.thread.daemon = True
+        self.thread.start()
+
+    def update(self):
+        # Read the next frame from the stream in a different thread
+        while True:
+            if self.capture.isOpened():
+                start_time = time.time()
+                (self.status, self.frame) = self.capture.read()
+                self.blurredFrame = cv2.GaussianBlur(self.frame, (5, 5), 0)
+                self.HSVFrame = cv2.cvtColor(self.blurredFrame, cv2.COLOR_BGR2HSV)
+                self.red_mask1 = cv2.inRange(self.HSVFrame, lowRed_HSV1, highRed_HSV1)
+                self.red_mask2 = cv2.inRange(self.HSVFrame, lowRed_HSV2, highRed_HSV2)
+
+                self.redFrame = cv2.bitwise_and(self.frame, self.frame, mask=self.red_mask1)
+                self.red_mask = self.red_mask1 | self.red_mask2
+                self.contours, self.hierarchy = cv2.findContours(self.red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+                for contour in self.contours:
+                    if cv2.contourArea(contour) > 1000:
+                        x, y, w, h = cv2.boundingRect(contour)
+                        cv2.rectangle(self.frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                FPS = int(1.0/(time.time()-start_time))
+                self.frame = cv2.putText(self.frame, f'FPS: {FPS}', [width - 120, 30], cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+            time.sleep(.01)
+
+    def show_frame(self):
+        # Display frames in main program
+        cv2.imshow('frame', self.frame)
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            self.capture.release()
+            cv2.destroyAllWindows()
+            exit(1)
 
 
-def func1(x):
-    print(f'func1-start{x}')
-    time.sleep(3)
-    print('func1-end')
-
-def func2(x):
-    print(f'func2-start{x}')
-    time.sleep(1)
-    print('func2-end')
-
-
-t1 = threading.Thread(target=func1, args=(14,))
-t2 = threading.Thread(target=func2, args=(22,))
-t1.start()
-t2.start()
-print(threading.activeCount())
-
-
+if __name__ == '__main__':
+    video_stream_widget = VideoStreamWidget()
+    while True:
+        try:
+            video_stream_widget.show_frame()
+        except AttributeError:
+            pass
