20692545ea91cec75f12df028a2b459f8b65b57b
bacElmore <71746770+bacElmore@users.noreply.github.com>
2022-02-16 22:28:22 -0800

    Merge TestTheading to ColorDetection - better threading logic

--- testGPU.py
+++ testGPU.py
@@ -5,3 +5,36 @@ def isThereGPU():
     print("Num CPUs Available: ", len(tf.config.list_physical_devices('CPU')))
 
 isThereGPU()
+
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+#model.fit(x_train, y_train, epochs=5)
+#model.evaluate(x_test, y_test)
+
+print(x_train.shape, y_train.shape)
+
+# pick a sample to plot
+sample = 3
+image = x_train[sample]
+# plot the sample
+fig = plt.figure
+plt.imshow(image, cmap='gray')
+plt.show()
+print(y_train[sample])
