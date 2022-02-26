c2af7053c5018edf3f73bbb2c6f947e8bf6e7db1
bactran <bac.tran@uselmore.com>
2022-02-25 21:21:31 -0800

    NorthStar NN testing

--- NorthStarFac-NN.py
+++ NorthStarFac-NN.py
@@ -1,3 +1,4 @@
+import keras.optimizers
 import tensorflow as tf
 
 def isThereGPU():
@@ -18,9 +19,11 @@ model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(28, 28)),
   tf.keras.layers.Dense(128, activation='relu'),
   tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(160, activation='relu'),
   tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(192, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(224, activation='relu'),
   tf.keras.layers.Dropout(0.2),
   tf.keras.layers.Dense(10, activation='softmax')
 ])
@@ -29,9 +32,9 @@ print(model.summary())
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+              metrics=['accuracy', tf.keras.metrics.RootMeanSquaredError()])
 
-model.fit(x_train, y_train, epochs=20)
+model.fit(x_train, y_train, batch_size=64, epochs=20)
 model.evaluate(x_test, y_test)
 
 # print(x_train.shape, y_train.shape)
