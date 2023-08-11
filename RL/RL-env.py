4fd14274ecd309b077e98cb4a122f978717c5df4
bactran1 <phongkiembac2202@gmail.com>
2023-08-10 22:24:35 -0700

    RL commit #@

--- RL/RL-env.py
+++ RL/RL-env.py
@@ -1,1 +1,28 @@
 #
+import gym
+from stable_baselines3 import A2C
+
+
+
+env = gym.make("LunarLander-v2", render_mode='human')
+
+
+env.reset()
+
+model = A2C("MlpPolicy", env, verbose=1)
+model.learn(total_timesteps=10000)
+
+episodes = 10
+
+for ep in range(episodes):
+
+    obs = env.reset()
+    done = False
+
+    while not done:
+        env.render()
+        obs, reward, done, info = env.step(env.action_space.sample())
+
+env.close()
+
+
